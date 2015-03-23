bdb6f771132a2fa32e71c42e6086e5a2e2f79fa5
accan83 <accan83@gmail.com>
2015-03-23 11:46:55 +0700

    New Connect File

--- handler/connection_handler.php
+++ handler/connection_handler.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost"; 
-$username = "root";
-$password = "";
-$database = "142_ppweb_r1";
+$server = "repo.stikom.edu"; 
+$username = "ppweb_r1_kel1";
+$password = "p455w0rd";
+$database = "db_online_printing";
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
