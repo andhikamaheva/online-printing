09a25b5b98b542d61c9b84c2803f4bc8aa849fa4
Andhika Maheva Wicaksono <andhikamaheva@gmail.com>
2015-04-07 13:07:01 +0700

    Delete File

--- admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	if(isset($_SESSION['admin'])){
-		include "template/template.php";
-	}else{
-		include "page/page_login.php";
-	}
-?>
--- admin/page/about.php
+++
@@ -1,8 +0,0 @@
-<div id="dashboard-header" class="row">
-	<div class="col-xs-10">
-		<h3>TENTANG APLIKASI</h3>
-	</div>
-</div>
-<div class="row-fluid">
-	Ini adalah project PPWeb Kelas R1 Semester 142 Kelompok 1 yang dibuat sejak bulan Februari 2015
-</div>
